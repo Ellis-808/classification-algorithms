a5415fb811df9c5e5c70166562f9f207e5b641aa
Zach Ellis <zachary.ellis@apricityhealth.com>
2020-11-19 16:30:03 -0600

    Split data into training and testing sets

--- src/Classification Algorithms.py
+++ src/Classification Algorithms.py
@@ -9,7 +9,7 @@
 # 
 # Source: https://www.kaggle.com/uciml/red-wine-quality-cortez-et-al-2009
 
-# In[1]:
+# In[2]:
 
 
 import numpy as np
@@ -19,6 +19,31 @@ import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.naive_bayes import GaussianNB
+
+
+# ### Preprocessing
+
+# In[16]:
+
+
+def train_test_split(df, training_size):
+    testing_set = df.sample(training_size)
+    df.drop(index=testing_set.index, inplace=True)
+    df.reset_index(drop=True, inplace=True)
+    
+    X_train = df.drop(columns="quality")
+    X_test = testing_set.drop(columns="quality").reset_index(drop=True)
+    
+    Y_train = df["quality"]
+    Y_test = testing_set["quality"].reset_index(drop=True)
+    
+    return X_train, X_test, Y_train, Y_test
+
+
+wine_data = pd.read_csv("../data/winequality-red.csv")
+print("\t\t\t\t\tRed Wine Data\n", wine_data)
+
+X_train, X_test, Y_train, Y_test = train_test_split(wine_data.copy(), 100)
 
 
 # ### Decision Tree Classifier
